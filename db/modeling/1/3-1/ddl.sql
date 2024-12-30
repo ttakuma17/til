614f3ec4aa391e900224739abc4fc6a72d9776e9
ttakuma17 <ttanimiya@gmail.com>
2024-12-30 12:14:06 +0900

    1-3のERD/DDL/DML

--- db/modeling/1/3-1/ddl.sql
+++ db/modeling/1/3-1/ddl.sql
@@ -3,8 +3,8 @@ CREATE TABLE Customers
     id         SERIAL PRIMARY KEY,
     name       VARCHAR(32) NOT NULL,
     tel        VARCHAR(15) NOT NULL,
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
+    create_date timestamp DEFAULT CURRENT_TIMESTAMP,
+    update_date timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Orders
@@ -12,29 +12,31 @@ CREATE TABLE Orders
     id           SERIAL PRIMARY KEY,
     paid         BOOLEAN NOT NULL,
     payment_total INTEGER NOT NULL,
-    customer_id  SERIAL,
-    created_at   timestamp DEFAULT CURRENT_TIMESTAMP,
-    updated_at   timestamp DEFAULT CURRENT_TIMESTAMP
+    customer_id   INTEGER,
+    create_date   timestamp DEFAULT CURRENT_TIMESTAMP,
+    update_date   timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE OrderDetails
 (
     id         SERIAL PRIMARY KEY,
-    quantity      INTEGER NOT NULL,
+    quantity   INTEGER NOT NULL,
+    subtotal   INTEGER NOT NULL,
     tax        INTEGER NOT NULL,
-    order_id   SERIAL,
-    product_id SERIAL,
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
+    order_id   INTEGER,
+    product_id INTEGER,
+    campaign_id INTEGER DEFAULT 0,
+    create_date timestamp DEFAULT CURRENT_TIMESTAMP,
+    update_date timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE OrderDetailsOptions
 (
     id         SERIAL PRIMARY KEY,
-    order_details_id   SERIAL,
-    option_id SERIAL,
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
+    order_details_id INTEGER,
+    option_id INTEGER,
+    create_date timestamp DEFAULT CURRENT_TIMESTAMP,
+    update_date timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Options
@@ -42,8 +44,19 @@ CREATE TABLE Options
     id                  SERIAL PRIMARY KEY,
     name                VARCHAR(32) NOT NULL,
     value               VARCHAR(32) NOT NULL,
-    created_at          timestamp DEFAULT CURRENT_TIMESTAMP,
-    updated_at          timestamp DEFAULT CURRENT_TIMESTAMP
+    create_date          timestamp DEFAULT CURRENT_TIMESTAMP,
+    update_date          timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Campaign
+(
+    id                  SERIAL PRIMARY KEY,
+    name                VARCHAR(32) NOT NULL,
+    discount_rate       INTEGER NOT NULL,
+    campaign_start      timestamp NOT NULL,
+    campaign_end        timestamp NOT NULL,
+    create_date         timestamp DEFAULT CURRENT_TIMESTAMP,
+    update_date         timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Products
@@ -52,26 +65,16 @@ CREATE TABLE Products
     name                VARCHAR(32) NOT NULL,
     price               INTEGER     NOT NULL,
     product_category_id SERIAL,
-    product_group_id     SERIAL,
-    created_at          timestamp DEFAULT CURRENT_TIMESTAMP,
-    updated_at          timestamp DEFAULT CURRENT_TIMESTAMP
+    create_date         timestamp DEFAULT CURRENT_TIMESTAMP,
+    update_date         timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE ProductCategory
 (
     id         SERIAL PRIMARY KEY,
     name       VARCHAR(32) NOT NULL,
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE ProductOptions
-(
-    id         SERIAL PRIMARY KEY,
-    product_id SERIAL,
-    option_id  SERIAL,
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
+    create_date timestamp DEFAULT CURRENT_TIMESTAMP,
+    update_date timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 ALTER TABLE Orders
@@ -79,23 +82,9 @@ ALTER TABLE Orders
         REFERENCES Customers (id)
         ON DELETE CASCADE;
 
--- Orderが消えたら、消したいのでON DELETE CASCADE
 ALTER TABLE OrderDetails
     ADD CONSTRAINT FK_OrderDetails_Orders FOREIGN KEY (order_id)
-        REFERENCES Orders (id)
-        ON DELETE CASCADE;
-
-ALTER TABLE Products
-    ADD CONSTRAINT FK_Products_ProductCategory FOREIGN KEY (product_category_id)
-        REFERENCES ProductCategory (id);
-
-ALTER TABLE ProductOptions
-    ADD CONSTRAINT FK_ProductOptions_Options FOREIGN KEY (option_id)
-        REFERENCES Options (id);
-
-ALTER TABLE ProductOptions
-    ADD CONSTRAINT FK_ProductOptions_Products FOREIGN KEY (product_id)
-        REFERENCES Products (id);
+        REFERENCES Orders (id);
 
 ALTER TABLE OrderDetailsOptions
     ADD CONSTRAINT FK_OrderDetailsOptions_OrdersDetail FOREIGN KEY (order_details_id)
@@ -104,3 +93,7 @@ ALTER TABLE OrderDetailsOptions
 ALTER TABLE OrderDetailsOptions
     ADD CONSTRAINT FK_OrderDetailsOptions_Options FOREIGN KEY (option_id)
         REFERENCES Options (id);
+
+ALTER TABLE Products
+    ADD CONSTRAINT FK_Products_ProductCategory FOREIGN KEY (product_category_id)
+        REFERENCES ProductCategory (id);
