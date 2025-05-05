cd4ec34ac6a6e37b79de3fe906e431cd43ebc9a8
ttakuma17 <ttanimiya@gmail.com>
2025-05-05 14:10:54 +0900

    Storyの追加

--- test/component-test/vite-tutorial/.storybook/preview.js
+++ test/component-test/vite-tutorial/.storybook/preview.js
@@ -1,10 +1,11 @@
 /** @type { import('@storybook/react').Preview } */
+import '../src/index.css';
 const preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
---
+++ test/component-test/vite-tutorial/src/stories/Game.stories.jsx
@@ -0,0 +1,8 @@
+import Game from '../App';
+
+export default {
+    title: 'TicTacToe/Game',
+    component: Game,
+};
+
+export const Default = {};
