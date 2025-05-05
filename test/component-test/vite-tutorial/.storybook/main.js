7aad9f259a2b2d49b54383cc87b6d0afc0e7864b
ttakuma17 <ttanimiya@gmail.com>
2025-05-05 16:30:16 +0900

    2-4の課題

--- test/component-test/vite-tutorial/.storybook/main.js
+++ test/component-test/vite-tutorial/.storybook/main.js
@@ -10,7 +10,7 @@ const config = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+    "@storybook/addon-interactions"
   ],
   "framework": {
     "name": "@storybook/react-vite",
--- test/component-test/vite-tutorial/src/stories/Game.stories.jsx
+++ test/component-test/vite-tutorial/src/stories/Game.stories.jsx
@@ -1,4 +1,6 @@
 import Game from '../App';
+import { within, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
 export default {
     title: 'TicTacToe/Game',
@@ -6,3 +8,19 @@ export default {
 };
 
 export const Default = {};
+
+export const XWins = {
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+
+        const squares = await canvas.findAllByRole('button', { name: '' });
+
+        await userEvent.click(squares[0]); // X
+        await userEvent.click(squares[3]); // O
+        await userEvent.click(squares[1]); // X
+        await userEvent.click(squares[4]); // O
+        await userEvent.click(squares[2]); // X
+
+        await expect(canvas.getByText(/Winner: X/)).toBeInTheDocument();
+    },
+};
